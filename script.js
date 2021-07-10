a1e5f74bc4dced7225eeda83727ca33f9a96a460
Kanishk Chhabra <67221487+mrkc2303@users.noreply.github.com>
2021-07-10 15:19:19 +0530

    Corrected All files

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
+var btn = document.getElementById("btn");
+var body = document.querySelector("body");
+
 function calculate(){
-            var p = 0;
-            var i = 0;
-            var n = 0;
-            var CI = 0;
+    var principal = 0;
+    var interest = 0;
+    var numberOfYear = 0;
+    var numberOfTimesInYear = 0;
+    var CI = 0;
 
-            p = parseInt(document.getElementById("principal").value);
-            i = parseInt(document.getElementById("annual-interest-rate").value);
-            n = parseInt(document.getElementById("number-of-period").value);
+    principal = parseInt(document.getElementById("principal").value);
+    interest = parseInt(document.getElementById("annual-interest-rate").value);
+    nyear = document.getElementById("number-of-year").value;
+    ntime = document.getElementById("number-of-times-in-year").value;
 
-            CI = ((p*(1+i)^n)-p);
-            document.getElementById("res").innerHTML=CI;
+    CI = (principal* Math.pow((1 + (interest/(ntime*100))), (ntime*nyear)));
+    CI = CI.toFixed(2);
+
+    document.getElementById("res").innerHTML = CI;
 }
+
+btn.addEventListener("click", calculate);
+body.addEventListener("keypress", function check(event) {
+    if(event.keyCode === 13){
+        calculate();
+    }
+});
